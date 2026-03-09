5381849d85ef2657e19ee57fb68734be03c75cce
Will4721 <williamlehd@gmail.com>
2026-03-09 12:36:16 +0100

    finish

---
+++ src/App/README.txt
@@ -0,0 +1,6 @@
+Things too improve:program
+chrasher hvis der er ingen ting i songs.txt
+loop en integer gennem list så man nemmere kan kalde på den rigtige
+youtube og spotify link virker ikke når der er mellemrum i sangen
+måske lav ads mere irriternde muligvis lave shuffle play aka den vælger aldrig sangen du leder efter
+
--- src/App/model/Spotifytest.java
+++ src/App/model/Spotifytest.java
@@ -15,6 +15,7 @@ import static App.Service.filehandler.file;
 import static App.Service.filehandler2.file2;
 
 public class Spotifytest {
+    //jeg ville gerne lave API som henter lyrics ned og lave en play funktion Api'en kommer fra Genius
     //private static final String ACCESS_TOKEN = "n8E4zoBg2crW5l3Sq6l2KK3funD_KTHyLnkjTpjz7WEkQOaqiCG2xeD2_n8U5ENr";
 public static void main(String[] args) {
 
@@ -94,7 +95,7 @@ public static void main(String[] args) {
             System.out.println("done");
             return 1;
         } else if (call == 5) {
-            System.out.println("What song do you want to remove");
+            System.out.println("What song do you want to edit");
             System.out.println(songs);
             System.out.println("type integer");
             int number = Integer.parseInt(sc.nextLine());
@@ -116,51 +117,7 @@ public static void main(String[] args) {
             int remove = Integer.parseInt(sc.nextLine());
             songs.remove(remove);
             return 1;
-        } //else if (call == 8) {
-            //System.out.print("Indtast sangtitel: ");
-            //String songTitle = sc.nextLine();
-
-            //try {
-                // Encode sangtitel korrekt (håndterer specialtegn og mellemrum)
-              //  String query = URLEncoder.encode(songTitle, StandardCharsets.UTF_8.toString());
-               // String apiUrl = "https://api.genius.com/search?q=" + query;
-
-                // Opret connection
-                //URL url = new URL(apiUrl);
-                //HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                //conn.setRequestMethod("GET");
-                //conn.setRequestProperty("Authorization", "Bearer " + ACCESS_TOKEN);
-
-                // Læs respons via InputStreamReader (vigtigt!)
-                //BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                //String inputLine;
-                //StringBuilder response = new StringBuilder();
-                //while ((inputLine = in.readLine()) != null) {
-                  //  response.append(inputLine);
-                //}
-                //in.close();
-
-                // Parse JSON (kræver at din json.jar er loadet i projektet)
-                //JSONObject json = new JSONObject(response.toString());
-                //JSONArray hits = json.getJSONObject("response").getJSONArray("hits");
-
-                //if (hits.length() > 0) {
-                  //  JSONObject firstHit = hits.getJSONObject(0).getJSONObject("result");
-                   // String songUrl = firstHit.getString("url");
-                   // System.out.println("Fandt sang: " + firstHit.getString("full_title"));
-                   // System.out.println("URL: " + songUrl);
-                //} else {
-                  //  System.out.println("Ingen resultater fundet for: " + songTitle);
-               // }
-
-           // } catch (UnsupportedEncodingException e) {
-             //   System.out.println("Fejl i tekst-kodning: " + e.getMessage());
-            //} catch (Exception e) {
-              //  System.out.println("Netværks- eller API fejl: " + e.getMessage());
-            //}
-           // return 1; // Returnerer til din menu-løkke
-
-    else if(call == 9){
+        } else if(call == 9){
         System.out.println("download by typing index number");
         int download = Integer.parseInt(sc.nextLine());
         System.out.println("you have downloaded: " + songs.get(download));
